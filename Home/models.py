b928befaf6222d8dec241436eebcebb08539b4e9
Ronish9165 <sthronesh11@gmail.com>
2022-05-25 21:06:16 +0545

    sptint two

--- Home/models.py
+++ Home/models.py
@@ -28,6 +28,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Blogs(models.Model):
+    blog_id=models.AutoField(auto_created=True,primary_key=True)
+    blog_name=models.CharField(max_length=200)
+    blog_details=models.CharField(max_length=200)
+    blog_image=models.FileField(upload_to='blog_image')
+
+    class Meta:
+        db_table="blog"
 
 
 # RATE_CHOICES = [
