ddb0e8bb6fd024e5f1ee73b23fb0ee392bc22eda
Nirajan gautam <78842151+nirajann@users.noreply.github.com>
2022-06-06 21:57:28 +0545

    testsprint2

--- Home/tests.py
+++ Home/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
-
+from unittest import result
+from django.urls import reverse,resolve
+from django.test import SimpleTestCase
+from Home.views import  home, productpage, SearchView, searchresult, edit
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+
+      # For blog
+    def test_case_blog_url(self):
+        url=reverse('blog')
+        self.assertEquals(resolve(url).func,edit)
+
+    # For blog_detail
+    def test_case_blog_detail_url(self):
+        url=reverse('blog_detail')
+        self.assertEquals(resolve(url).func,edit)
+
+      # For categorie
+    def test_case_categorie_detail_url(self):
+        url=reverse('categorie')
+        self.assertEquals(resolve(url).func,edit)
+    
+    # For signuplogin
+    def test_case_signuplogin_detail_url(self):
+        url=reverse('signuplogin')
+        self.assertEquals(resolve(url).func,edit)
+
+    # For contact
+    def test_case_contact_detail_url(self):
+        url=reverse('contact')
+        self.assertEquals(resolve(url).func,edit)
+    
