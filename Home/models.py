408ea2a506de6c9509f5c6cd430076a21a0eabb9
Nirajan gautam <78842151+nirajann@users.noreply.github.com>
2022-06-12 21:07:28 +0545

    sprint4

--- Home/models.py
+++ Home/models.py
@@ -168,9 +168,11 @@ class CheckOut(View):
 
 class Room(models.Model):
     name = models.CharField(max_length=1000)
+
+
 class Message(models.Model):
     value = models.CharField(max_length=1000000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
     user = models.CharField(max_length=1000000)
     room = models.CharField(max_length=1000000)        
 
