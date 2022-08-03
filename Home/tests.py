599717c83a7dc4625bbf44a8fa2d04dd920e5de6
Nirajan gautam <78842151+nirajann@users.noreply.github.com>
2022-08-03 14:41:45 +0545

    deploy

--- Home/tests.py
+++ Home/tests.py
@@ -1,7 +1,6 @@
-
 from django.urls import resolve , reverse
 from django.test import SimpleTestCase 
-from Home.views import  creatorprofile, categorie, contact, productform, showblog
+from Home.views import  *
 # Create your tests here.
 class TestUrls(SimpleTestCase):
 
@@ -10,22 +9,71 @@ class TestUrls(SimpleTestCase):
         url=reverse('blog')
         self.assertEquals(resolve(url).func,showblog)
 
-    # For creatorprofile
-    def test_case_blog_detail_url(self):
+    # For creatorprofile week 6
+    def test_case_creator_profile_url(self):
         url=reverse('creatorprofile')
         self.assertEquals(resolve(url).func,creatorprofile)
 
-      # For categorie
+    #   # For categorie 
     def test_case_categorie_detail_url(self):
         url=reverse('categorie')
         self.assertEquals(resolve(url).func,categorie)
     
-    # For signuplogin
-    def test_case_signuplogin_detail_url(self):
+    # # For contact
+    def test_case_contact_url(self):
         url=reverse('contact')
         self.assertEquals(resolve(url).func,contact)
 
-    # For contact
-    def test_case_contact_detail_url(self):
-        url=reverse('productform')
-        self.assertEquals(resolve(url).func,productform)
+    # # For store week 
+    def test_case_store_url(self):
+        url=reverse('store')
+        self.assertEquals(resolve(url).func,store)
+
+    # # For contact week 5
+    def test_case_profile_url(self):
+        url=reverse('profile')
+        self.assertEquals(resolve(url).func,profile)
+
+     # # For contact week 6
+    def test_case_creator_url(self):
+        url=reverse('creator')
+        self.assertEquals(resolve(url).func,creator)
+
+     # # For contact week 4
+    def test_case_verify_payment_url(self):
+        url=reverse('verify_payment')
+        self.assertEquals(resolve(url).func,verify_payment)
+
+     #for cart week 4
+    def test_case_delete_cart_url(self):
+        url=reverse('delete_cart',args=[1])
+        self.assertEquals(resolve(url).func, delete_cart)
+
+    def test_case_logoutcreator_url(self):
+        url=reverse('logoutcreator')
+        self.assertEquals(resolve(url).func, logoutcreator)
+    
+    #For Profile week 3
+    def test_case_profile_url(self):
+        url=reverse('profile')
+        self.assertEquals(resolve(url).func, profile)
+
+    # For search week 3
+    def test_case_search_url(self):
+        url=reverse('search')
+        self.assertEquals(resolve(url).func, SearchView)    
+
+    # For search week 6
+    def test_case_logoutcreator_url(self):
+        url=reverse('logoutcreator')
+        self.assertEquals(resolve(url).func, logoutcreator)
+
+    # For search week 7
+    def test_case_admindashboard_url(self):
+        url=reverse('admindashboard')
+        self.assertEquals(resolve(url).func, admin_dashboard_view)
+
+     # For search week 7
+    def test_case_admindashboard_url(self):
+        url=reverse('admindashboard')
+        self.assertEquals(resolve(url).func, admin_dashboard_view)
